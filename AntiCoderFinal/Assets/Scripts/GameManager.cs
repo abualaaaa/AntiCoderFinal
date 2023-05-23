65de26b6fc8f394ed58f6c80bb103637aa0edfeb
753037 <753037@student.dusd.net>
2023-05-23 12:16:30 -0700

    created titlew sccreen

--- AntiCoderFinal/Assets/Scripts/GameManager.cs
+++ AntiCoderFinal/Assets/Scripts/GameManager.cs
@@ -7,17 +7,24 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public Button nextButton;
+    public GameObject titleScreen;
+    public GameObject pauseScreen;
+    private bool paused;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Check if the user has pressed the P key
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+        }
     }
 
     public void NextLevel()
@@ -28,5 +35,26 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         nextButton.gameObject.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        titleScreen.gameObject.SetActive(false);
+    }
+
+    void ChangePaused()
+    {
+        if (!paused)
+        {
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 }
--- AntiCoderFinal/Assets/Scripts/Next Button.cs
+++ AntiCoderFinal/Assets/Scripts/Next Button.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NextButton : MonoBehaviour
 {
-    public Button nextButton;
+   public Button button;
     
    private GameManager gameManager;
 
